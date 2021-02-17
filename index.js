7f2a9d8c477e8d932b699aa7fd91c0ec2fdf0bc9
Darl <dashaapril93@gmail.com>
2021-02-17 21:38:32 +0300

    add const sections

---
+++ index.js
@@ -0,0 +1,9 @@
+const sections = [
+    {type: 'title', value:'Hello World'},
+    {type: 'text', value: loremText},
+    {type: 'columns', value: [loremText, loremText, loremText]},
+    
+
+];
+
+console.log(sections);
